bded78289f6071d10f1bc1714b33f9bec8c03386
Sergey Rozhkov <ppcinside@gmail.com>
2017-05-13 05:17:05 +0500

    v2

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=360px, initial-scale=1">
+	  <title>Sergey Rozhkov Homepage</title>
+	  <link rel="stylesheet" href="css/main.css" />
+	  <link href='https://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+	  <script src="js/main.js"></script> -->
+	  <style type="text/css">.playerbutton{cursor:pointer;border:none;background:transparent;outline:none;display:inline}.gaf210codes_youtubebox,.preload_box{width:1px;height:1px;overflow:hidden;visibility:hidden;position:absolute;display:inline}.gaf210codes_youtubetitle{font-family:Arial;font-size:12px;color: #fff;display:inline-block;vertical-align:top;padding:6px;padding-bottom:0px;text-shadow:1px 1px #ccc;}#gaf210codes_3J8mw_container{}</style>
+	  <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+	  <script type="text/javascript">function toggleButtonPlayer_3J8mw(buttonobj){buttonobj.style.display="none";if(buttonobj.id=="gaf210codes-3J8mw-playbutton"){document.getElementById("gaf210codes-3J8mw-pausebutton").style.display="inline";}else{document.getElementById("gaf210codes-3J8mw-playbutton").style.display="inline";}}/*Youtube player code generated @ http://gaf210.imvustylez.net*/</script>
+	  <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-98176224-1', 'auto');
+		  ga('send', 'pageview');
+		  </script>
+	</head>
+
+	<body>
+		<div id="gaf210codes_3J8mw_container"><div class="gaf210codes_youtubebox"><iframe width="250" height="250" src="https://www.youtube.com/embed/?list=PLIkNOQekcN17Z34PiWd2mVKLkG2a7qXoX&amp;listType=playlist&amp;enablejsapi=1&amp;fs=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;controls=0&amp;autoplay=1&amp;loop=1&amp;index=<?php print(rand(1,14)) ?>" onload="gaf210codes_3J8mw=new YT.Player(this,{events:{'onStateChange':function(event){if(event.data == 1){document.getElementById('gaf210codes_3J8mw_title').innerHTML=event.target.getVideoData().title}}}})"></iframe></div>
+		<button type="button" class="playerbutton" id="gaf210codes-3J8mw-playbutton" onclick="gaf210codes_3J8mw.playVideo();toggleButtonPlayer_3J8mw(this)" style="display:none"><img src="http://i62.tinypic.com/axnot0.jpg"></button><button type="button" class="playerbutton" id="gaf210codes-3J8mw-pausebutton" onclick="gaf210codes_3J8mw.pauseVideo();toggleButtonPlayer_3J8mw(this)"><img src="http://i57.tinypic.com/2mqugw2.jpg"></button><div class="preload_box"><img src="http://i62.tinypic.com/axnot0.jpg"><img src="http://i57.tinypic.com/2mqugw2.jpg"></div><input style="outline:none" type="range" min="0" max="100" onchange="gaf210codes_3J8mw.setVolume(this.value)"><div id="gaf210codes_3J8mw_title" class="gaf210codes_youtubetitle"></div></div>
+		<div class="screen center">
+			<!-- <h1>**** COMMODORE 64 BASIC V2 ****</h1>
+    		<h2>64K RAM SYSTEM 38911 BASIC BYTES FREE</h2>
+		    <p>READY.</p> -->
+			<div class="info">
+				<!-- <div class="img"><img src="/img/amiga.png" /></div> -->
+				<h1 class="glitch" data-text="hello">hello<br></h1>
+				<!-- <div class="info_subtitle">dev/design</div> -->
+				<div class="links">
+					<a href="mailto:ppcinside@gmail.com" subject="Hi!">ppcinside@gmail.com</a><br>
+					<a href="http://vk.com/greyxp" target="_blank">vk.com/greyxp</a><br>
+					<a href="http://ppcinside.tumblr.com" target="_blank">ppcinside.tumblr.com</a><br>
+					<a href="http://last.fm/user/ppcinside" target="_blank">last.fm/user/ppcinside</a><br>
+					<a href="http://instagram.com/greyxp" target="_blank">instagram.com/greyxp</a><br>
+				</div>
+				<div class="cursor"></div>
+			</div>
+		</div>
+	</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 var colors = new Array(
-  [62,35,255],
   [60,255,60],
   [255,35,98],
   [45,175,230],
@@ -8,7 +7,7 @@ var colors = new Array(
   [255,128,0]);
 
 var step = 0;
-//color table indices for: 
+//color table indices for:
 // current color left
 // next color left
 // current color right
@@ -16,13 +15,13 @@ var step = 0;
 var colorIndices = [0,1,2,3];
 
 //transition speed
-var gradientSpeed = 0.002;
+var gradientSpeed = 0.0001;
 
 function updateGradient()
 {
-  
+
   if ( $===undefined ) return;
-  
+
 var c0_0 = colors[colorIndices[0]];
 var c0_1 = colors[colorIndices[1]];
 var c1_0 = colors[colorIndices[2]];
@@ -40,21 +39,21 @@ var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
 var color2 = "rgb("+r2+","+g2+","+b2+")";
 
  $(document.body).css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color1+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color1+" 100%)"});
+
   step += gradientSpeed;
   if ( step >= 1 )
   {
     step %= 1;
     colorIndices[0] = colorIndices[1];
     colorIndices[2] = colorIndices[3];
-    
+
     //pick two new target color indices
     //do not pick the same as the current one
     colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
     colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    
+
   }
 }
 
@@ -92,7 +91,7 @@ var clear = function() {
   context.rect(0, 0, w, h);
   context.fill();
 };
-    
+
 var glitchImg = function() {
   for (var i = 0; i < randInt(1, 13); i++) {
     var x = Math.random() * w;
